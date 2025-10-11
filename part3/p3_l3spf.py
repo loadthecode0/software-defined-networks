10e415dc9f2f945f8b9dfeccee75df114fcf8cef
loadthecode0 <artyaastha@gmail.com>
2025-10-11 15:56:41 +0530

    remove comments from p3 app

--- part3/p3_l3spf.py
+++ part3/p3_l3spf.py
@@ -99,64 +99,6 @@ class L3ShortestPath(app_manager.RyuApp):
         dp.send_msg(out)
         self.logger.info("Sent ICMP Echo Reply for %s from %s", my_ip, s_name)
         return True
-    # # self switch reply 
-    # def _handle_icmp_request(self, dp, eth, ip_pkt, in_port):
-    #     """
-    #     Handles an ICMP Echo Request destined for the switch's own IP.
-    #     Constructs and sends an ICMP Echo Reply.
-    #     """
-    #     parser = dp.ofproto_parser
-    #     ofproto = dp.ofproto
-        
-    #     # Check if it's an Echo Request
-    #     icmp_pkt = ip_pkt.get_protocol(icmp.icmp)
-    #     if not icmp_pkt or icmp_pkt.type != 8: # 8 is Echo Request
-    #         return False
-
-    #     # Find the switch interface IP that was the target of the ping
-    #     my_ip = ip_pkt.dst
-    #     my_mac = None
-    #     s_name = self.find_router_for_ip(my_ip)
-    #     if not s_name: return False
-        
-    #     switch_config = self.switches.get(s_name)
-    #     for iface in switch_config.get("interfaces", []):
-    #         if iface.get("ip") == my_ip:
-    #             my_mac = iface.get("mac")
-    #             break
-        
-    #     if not my_mac: return False
-
-    #     # Construct Echo Reply packet
-    #     p = packet.Packet()
-    #     p.add_protocol(ethernet.ethernet(ethertype=eth.ethertype,
-    #                                       dst=eth.src,
-    #                                       src=my_mac))
-    #     p.add_protocol(ipv4.ipv4(dst=ip_pkt.src,
-    #                              src=my_ip,
-    #                              proto=ip_pkt.proto))
-    #     p.add_protocol(icmp_pkt.__class__(type_=0, # 0 is Echo Reply
-    #                                       code=0,
-    #                                       csum=0,
-    #                                       data=icmp_pkt.data))
-        
-    #     # Send the reply via PacketOut
-    #     p.serialize()
-    #     actions = [parser.OFPActionOutput(port=in_port)]
-    #     out = parser.OFPPacketOut(datapath=dp,
-    #                               buffer_id=ofproto.OFP_NO_BUFFER,
-    #                               in_port=ofproto.OFPP_CONTROLLER,
-    #                               actions=actions,
-    #                               data=p.data)
-    #     dp.send_msg(out)
-    #     self.logger.info("Sent ICMP Echo Reply for %s from %s", my_ip, s_name)
-    #     return True
-
-
-
-
-
-
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -173,7 +115,7 @@ class L3ShortestPath(app_manager.RyuApp):
         self.logger.info("Switch %s connected", dpid)
 
     # --- Packet-in handler --------------------------------------------------
-# REPLACE this entire function
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
         msg = ev.msg
@@ -198,32 +140,6 @@ class L3ShortestPath(app_manager.RyuApp):
         if ip_pkt:
             # MODIFIED: Pass the full 'pkt' object
             self.handle_ipv4(dp, in_port, pkt, eth, ip_pkt)
-    # @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    # def packet_in_handler(self, ev):
-    #     msg = ev.msg
-    #     dp = msg.datapath
-    #     dpid = dp.id
-    #     parser = dp.ofproto_parser
-    #     ofproto = dp.ofproto
-    #     in_port = msg.match["in_port"]
-
-    #     pkt = packet.Packet(msg.data)
-    #     eth = pkt.get_protocol(ethernet.ethernet)
-
-    #     if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-    #         return
-
-    #     # Handle ARP
-    #     arp_pkt = pkt.get_protocol(arp.arp)
-    #     if arp_pkt:
-    #         if arp_pkt.opcode == arp.ARP_REQUEST:
-    #             self.handle_arp(dp, in_port, eth, arp_pkt)
-    #         return
-
-    #     # Handle IPv4
-    #     ip_pkt = pkt.get_protocol(ipv4.ipv4)
-    #     if ip_pkt:
-    #         self.handle_ipv4(dp, in_port, msg, eth, ip_pkt)
 
     # --- ARP reply ----------------------------------------------------------
     def handle_arp(self, dp, in_port, eth, arp_pkt):
@@ -251,26 +167,6 @@ class L3ShortestPath(app_manager.RyuApp):
                     dp.send_msg(out)
                     self.logger.info("Replied to ARP for %s from %s", dst_ip, i["mac"])
                     return
-
-    # --- IPv4 routing -------------------------------------------------------
-    # def handle_ipv4(self, dp, in_port, msg, eth, ip_pkt):
-    #     if self._handle_icmp_request(dp, eth, ip_pkt, in_port):
-    #         return # If it was handled, we are done.
-    #     src_ip, dst_ip = ip_pkt.src, ip_pkt.dst
-
-    #     src_router = self.find_router_for_ip(src_ip)
-    #     dst_router = self.find_router_for_ip(dst_ip)
-
-    #     if not src_router or not dst_router:
-    #         self.logger.warning("Unknown subnet for %s -> %s", src_ip, dst_ip)
-    #         return
-
-    #     path = nx.shortest_path(self.graph, src_router, dst_router, weight="weight")
-    #     self.logger.info("Path %s -> %s : %s", src_ip, dst_ip, path)
-
-    #     # Install bidirectional flows
-    #     self.install_path(path, ip_pkt.dst)
-    #     self.install_path(list(reversed(path)), ip_pkt.src)
 
     def handle_ipv4(self, dp, in_port, pkt, eth, ip_pkt):
         # Check if the packet is an ICMP request for the switch itself
@@ -373,180 +269,4 @@ class L3ShortestPath(app_manager.RyuApp):
             self.add_flow(dp, 10, match, actions)
             self.logger.info("Installed L3 flow on %s for dst %s -> port %d (MACs %s -> %s)",
                             curr_switch, dst_ip, out_port, src_mac, dst_mac)
-
-
-    # def install_path(self, path, dst_ip):
-    #     """
-    #     Install L3 flows along a path for a given destination IP.
-    #     Handles switch-to-switch hops and final hop to host.
-    #     """
-    #     for i in range(len(path)):
-    #         curr_switch = path[i]
-    #         dp = next((d for d in self.datapaths.values() if d.id == self.switches[curr_switch]["dpid"]), None)
-    #         if not dp:
-    #             continue
-    #         parser = dp.ofproto_parser
-
-    #         # Determine next hop
-    #         if i < len(path) - 1:
-    #             next_hop = path[i+1]
-    #         else:
-    #             next_hop = dst_ip  # final hop to host
-
-    #         # Determine output port and MAC addresses
-    #         if dst_ip in self.hosts:
-    #             # Final hop to host
-    #             host_entry = self.hosts[dst_ip]
-    #             host_name = host_entry["name"]
-    #             dst_host_mac = host_entry["mac"]
-
-    #             # Find interface facing the host (neighbor == host name)
-    #             out_iface = next(
-    #                 (iface for iface in self.switches[curr_switch]["interfaces"]
-    #                  if iface.get("neighbor") == host_name),
-    #                 None
-    #             )
-
-    #             if not out_iface:
-    #                 self.logger.warning("No host-facing interface on %s for host %s (%s)",
-    #                                     curr_switch, host_name, dst_ip)
-    #                 continue
-
-    #             out_port = int(out_iface["name"].split("eth")[-1])
-    #             src_mac = out_iface["mac"]
-    #             dst_mac = dst_host_mac
-    #         else:
-    #             # Next hop is another switch
-    #             out_iface = next((iface for iface in self.switches[curr_switch]["interfaces"]
-    #                             if iface.get("neighbor") == next_hop), None)
-    #             in_iface_next = next((iface for iface in self.switches[next_hop]["interfaces"]
-    #                                 if iface.get("neighbor") == curr_switch), None)
-    #             if not out_iface or not in_iface_next:
-    #                 self.logger.warning("Missing inter-switch iface for %s -> %s", curr_switch, next_hop)
-    #                 continue
-    #             out_port = int(out_iface["name"].split("eth")[-1])
-    #             src_mac = out_iface["mac"]
-    #             dst_mac = in_iface_next["mac"]
-
-
-            # Determine output port and MAC addresses
-    #         if next_hop in self.hosts:
-    #             # Last hop to host
-    #             dst_host_mac = self.hosts[dst_ip]["mac"]
-    #             out_iface = next((iface for iface in self.switches[curr_switch]["interfaces"]
-    #                             if ipaddress.ip_address(dst_ip) in ipaddress.ip_network(iface["subnet"])), None)
-    #             if not out_iface: 
-    #                 continue
-    #             out_port = int(out_iface["name"].split("eth")[-1])
-    #             src_mac = out_iface["mac"]
-    #             dst_mac = dst_host_mac
-    #         else:
-    #             # Next hop is a switch
-    #             out_iface = next((iface for iface in self.switches[curr_switch]["interfaces"]
-    #                             if iface.get("neighbor") == next_hop), None)
-    #             in_iface_next = next((iface for iface in self.switches[next_hop]["interfaces"]
-    #                                 if iface.get("neighbor") == curr_switch), None)
-    #             if not out_iface or not in_iface_next: 
-    #                 continue
-    #             out_port = int(out_iface["name"].split("eth")[-1])
-    #             src_mac = out_iface["mac"]
-    #             dst_mac = in_iface_next["mac"]
-
-    #         # Create match and actions
-    #         match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=dst_ip)
-    #         actions = [
-    #             parser.OFPActionSetField(eth_src=src_mac),
-    #             parser.OFPActionSetField(eth_dst=dst_mac),
-    #             parser.OFPActionDecNwTtl(),
-    #             parser.OFPActionOutput(out_port)
-    #         ]
-
-    #         # Install the flow
-    #         self.add_flow(dp, 10, match, actions)
-    #         self.logger.info("Installed L3 flow on %s for dst %s -> port %d (MACs %s -> %s)",
-    #                         curr_switch, dst_ip, out_port, src_mac, dst_mac)
-
-
-
-
-    # # def install_path(self, path, dst_ip):
-    # #     # Find the destination host's information from the config
-    # #     dst_host_mac = self.hosts.get(dst_ip, {}).get("mac")
-    # #     if not dst_host_mac:
-    # #         self.logger.error("Destination host %s not in config", dst_ip)
-    # #         return
-
-    # #     # Install flows for each hop in the path
-    # #     for i in range(len(path)):
-    # #         s_name = path[i]
-    # #         sw = self.switches[s_name]
-    # #         dp = next((d for d in self.datapaths.values() if d.id == sw["dpid"]), None)
-    # #         if not dp:
-    # #             continue
-
-    # #         parser = dp.ofproto_parser
-            
-    # #         # Determine the next hop and find the correct MACs and output port
-    # #         if i < len(path) - 1:
-    # #             # This is an intermediate switch-to-switch hop
-    # #             next_s_name = path[i+1]
-                
-    # #             # Find the interface on the current switch facing the next switch
-    # #             out_iface = next((iface for iface in sw["interfaces"] if iface.get("neighbor") == next_s_name), None)
-    # #             if not out_iface: continue
-                
-    # #             # Find the interface on the next switch that connects back to the current one
-    # #             next_sw_config = self.switches[next_s_name]
-    # #             in_iface_next_hop = next((iface for iface in next_sw_config["interfaces"] if iface.get("neighbor") == s_name), None)
-    # #             if not in_iface_next_hop: continue
-                
-    # #             out_port = int(out_iface["name"].split("eth")[-1])
-    # #             new_src_mac = out_iface["mac"]
-    # #             new_dst_mac = in_iface_next_hop["mac"]
-
-    # #         else:
-    # #             # This is the final hop from the last switch to the destination host
-                
-    # #             # Find the interface on the last switch connected to the destination host's subnet
-    # #             dst_host_ip_obj = ipaddress.ip_address(dst_ip)
-    # #             out_iface = next((iface for iface in sw["interfaces"] if dst_host_ip_obj in ipaddress.ip_network(iface["subnet"])), None)
-    # #             if not out_iface: continue
-                
-    # #             out_port = int(out_iface["name"].split("eth")[-1])
-    # #             new_src_mac = out_iface["mac"]
-    # #             new_dst_mac = dst_host_mac # The final destination MAC is the host's MAC
-            
-    # #         # Define the match and actions for the flow rule
-    # #         match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=dst_ip)
-    # #         actions = [
-    # #             parser.OFPActionSetField(eth_src=new_src_mac),
-    # #             parser.OFPActionSetField(eth_dst=new_dst_mac),
-    # #             parser.OFPActionDecNwTtl(),
-    # #             parser.OFPActionOutput(out_port)
-    # #         ]
-    # #         self.add_flow(dp, 10, match, actions)
-    # #         self.logger.info("Installed L3 flow on %s for dst %s -> port %d (MACs %s -> %s)", 
-    # #                          s_name, dst_ip, out_port, new_src_mac, new_dst_mac)
-
-
-
-
-    # # def install_path(self, path, dst_ip):
-    # #     for i in range(len(path) - 1):
-    # #         s1, s2 = path[i], path[i + 1]
-    # #         sw = self.switches[s1]
-    # #         out_port = None
-    # #         for iface in sw["interfaces"]:
-    # #             if iface["neighbor"] == s2:
-    # #                 out_port = int(iface["name"].split("eth")[-1])
-    # #                 break
-    # #         if not out_port:
-    # #             continue
-    # #         dp = next((d for d in self.datapaths.values() if d.id == sw["dpid"]), None)
-    # #         if dp:
-    # #             parser = dp.ofproto_parser
-    # #             match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=dst_ip)
-    # #             actions = [parser.OFPActionOutput(out_port)]
-    # #             self.add_flow(dp, 10, match, actions)
-    # #             self.logger.info("Installed flow on %s for dst %s -> port %s", s1, dst_ip, out_port)
 

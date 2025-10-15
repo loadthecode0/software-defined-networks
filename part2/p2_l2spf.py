15483b83d31348ed77ac3a566f9faa8a4a0c1604
loadthecode0 <artyaastha@gmail.com>
2025-10-15 09:07:38 +0530

    add tcp and ip headers for flow rules

--- part2/p2_l2spf.py
+++ part2/p2_l2spf.py
@@ -16,6 +16,7 @@ import random
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
 from ryu.lib.packet import packet, ethernet, ether_types
+from ryu.lib.packet import ipv4, tcp
 
 import time
 
@@ -30,7 +31,16 @@ class ShortestPathController(BaseSPController):
             return random.choice(all_paths)
         return all_paths[0] # if not ecmp, choose the first, deterministically
 
-    def install_path_flows(self, path: List[str], src_mac=None, dst_mac=None):
+    def install_path_flows(
+        self,
+        path: List[str],
+        src_mac=None,
+        dst_mac=None,
+        src_ip=None,
+        dst_ip=None,
+        src_port=None,
+        dst_port=None
+        ):
         """
         Install OpenFlow flows for src_mac <-> dst_mac along the given path.
         - path: list of switch names, e.g. ['s1','s2','s4','s6']
@@ -72,7 +82,18 @@ class ShortestPathController(BaseSPController):
                 continue
 
             # forward match/action on this switch
-            match_fwd = parser.OFPMatch(eth_src=src_mac, eth_dst=dst_mac)
+            # match_fwd = parser.OFPMatch(eth_type=0x0800,eth_src=src_mac, eth_dst=dst_mac, ip_proto=6, tcp_src=src_port, tcp_dst=dst_port)
+            # Base L2 match
+            fwd_match_kwargs = dict(eth_src=src_mac, eth_dst=dst_mac)
+
+            # Add IP and TCP fields if present
+            if src_ip and dst_ip:
+                fwd_match_kwargs.update(eth_type=0x0800, ipv4_src=src_ip, ipv4_dst=dst_ip)
+            if src_port and dst_port:
+                fwd_match_kwargs.update(ip_proto=6, tcp_src=src_port, tcp_dst=dst_port)
+
+            match_fwd = parser.OFPMatch(**fwd_match_kwargs)
+
             actions_fwd = [parser.OFPActionOutput(out_port)]
             self.add_flow(dp, priority=1, match=match_fwd, actions=actions_fwd)
 
@@ -88,12 +109,19 @@ class ShortestPathController(BaseSPController):
             if rev_out is None:
                 self.logger.warning("No reverse port known for s%s when installing reverse flow", cur)
             else:
-                match_rev = parser.OFPMatch(eth_src=dst_mac, eth_dst=src_mac)
+                # match_rev = parser.OFPMatch(eth_type=0x0800,eth_src=dst_mac, eth_dst=src_mac, ip_proto=6, tcp_src=dst_port, tcp_dst=src_port)
+                match_rev_kwargs = dict(eth_src=dst_mac, eth_dst=src_mac)
+                if src_ip and dst_ip:
+                    match_rev_kwargs.update(eth_type=0x0800, ipv4_src=dst_ip, ipv4_dst=src_ip)
+                if src_port and dst_port:
+                    match_rev_kwargs.update(ip_proto=6, tcp_src=dst_port, tcp_dst=src_port)
+
+                match_rev = parser.OFPMatch(**match_rev_kwargs)
                 actions_rev = [parser.OFPActionOutput(rev_out)]
                 self.add_flow(dp, priority=1, match=match_rev, actions=actions_rev)
 
             self.logger.info("s%s: installed %s->%s out:%s and reverse out:%s", cur, src_mac, dst_mac, out_port, rev_out)
-            time.sleep(1)
+            # time.sleep(1)
 
         # Install rule on the *destination switch* to forward to host port (if not same as previous step)
         final_switch = dpids[-1]
@@ -108,7 +136,8 @@ class ShortestPathController(BaseSPController):
             if dst_host_port is None:
                 self.logger.warning("No host port known for destination host %s; cannot install final rule", dst_mac)
             else:
-                match_fwd_final = parser.OFPMatch(eth_src=src_mac, eth_dst=dst_mac)
+                # match_fwd_final = parser.OFPMatch(eth_type=0x0800,eth_src=src_mac, eth_dst=dst_mac, ip_proto=6, tcp_src=src_port, tcp_dst=dst_port)
+                match_fwd_final = parser.OFPMatch(**fwd_match_kwargs)
                 actions_fwd_final = [parser.OFPActionOutput(dst_host_port)]
                 self.add_flow(dp_final, priority=1, match=match_fwd_final, actions=actions_fwd_final)
                 self.logger.info("s%s: installed final forward %s->%s out:%s", final_switch, src_mac, dst_mac, dst_host_port)
@@ -120,7 +149,14 @@ class ShortestPathController(BaseSPController):
                 if rev_out is None:
                     self.logger.warning("No reverse port on final switch s%s to previous s%s", final_switch, prev)
                 else:
-                    match_rev_final = parser.OFPMatch(eth_src=dst_mac, eth_dst=src_mac)
+                    # match_rev_final = parser.OFPMatch(eth_type=0x0800,eth_src=dst_mac, eth_dst=src_mac, ip_proto=6, tcp_src=dst_port, tcp_dst=src_port)
+                    match_rev_kwargs = dict(eth_src=dst_mac, eth_dst=src_mac)
+                    if src_ip and dst_ip:
+                        match_rev_kwargs.update(eth_type=0x0800, ipv4_src=dst_ip, ipv4_dst=src_ip)
+                    if src_port and dst_port:
+                        match_rev_kwargs.update(ip_proto=6, tcp_src=dst_port, tcp_dst=src_port)
+
+                    match_rev_final = parser.OFPMatch(**match_rev_kwargs)
                     actions_rev_final = [parser.OFPActionOutput(rev_out)]
                     self.add_flow(dp_final, priority=1, match=match_rev_final, actions=actions_rev_final)
                     self.logger.info("s%s: installed final reverse %s->%s out:%s", final_switch, dst_mac, src_mac, rev_out)
@@ -138,6 +174,35 @@ class ShortestPathController(BaseSPController):
         eth = pkt.get_protocol(ethernet.ethernet)
         if eth.ethertype == ether_types.ETH_TYPE_LLDP: # setup packet, handled separately in base class
             return
+
+        # after parsing ethernet
+        ip_pkt = pkt.get_protocol(ipv4.ipv4)
+        # ip_pkt=None
+        tcp_pkt = pkt.get_protocol(tcp.tcp)
+
+        # if not tcp_pkt:
+        # #     # don't install path yet; just flood until TCP is seen
+        # #     actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+        # #     self.logger.info("[FLOOD]")
+        # #     out = parser.OFPPacketOut(
+        # #         datapath=dp, buffer_id=msg.buffer_id, in_port=in_port,
+        # #         actions=actions, data=msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
+        # #     )
+        # #     dp.send_msg(out)
+        #     return
+
+        src_ip = dst_ip = None
+        src_port = dst_port = None
+
+        if ip_pkt:
+            self.logger.info("IP detected")
+            src_ip = ip_pkt.src
+            dst_ip = ip_pkt.dst
+
+        if tcp_pkt:
+            self.logger.info("TCP detected")
+            src_port = tcp_pkt.src_port
+            dst_port = tcp_pkt.dst_port
 
         src, dst = eth.src, eth.dst # src and dst hosts
         dpid = dp.id # current switch
@@ -163,7 +228,7 @@ class ShortestPathController(BaseSPController):
             return
 
         # here, we know src & dst switches => compute path
-        dst_dpid, dst_port = self.host_location[dst]
+        dst_dpid, dst_host_port = self.host_location[dst]
         src_switch = f"s{dpid}" # current switch
         dst_switch = f"s{dst_dpid}" # switch on which dst host lives
         all_paths = self.graph.dijkstra_all_shortest_paths(src_switch, dst_switch)
@@ -171,7 +236,16 @@ class ShortestPathController(BaseSPController):
 
         if path:
             self.logger.info(f"[INSTALL] from switch {dpid}")
-            self.install_path_flows(path, src_mac=src, dst_mac=dst)
+            # self.install_path_flows(path, src_mac=src, dst_mac=dst)
+            self.install_path_flows(
+                path,
+                src_mac=src,
+                dst_mac=dst,
+                src_ip=src_ip,
+                dst_ip=dst_ip,
+                src_port=src_port,
+                dst_port=dst_port
+            )
 
         # Also send this first packet along the path immediately
         actions = [parser.OFPActionOutput(ofproto.OFPP_TABLE)]
